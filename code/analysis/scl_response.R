c903fe54c27d70819644782431b34bc68a7d232d
Joel larwood <joel.larwood@uq.net.au>
2021-02-03 16:56:17 +1000

    SCL processed

--- code/analysis/scl_response.R
+++ code/analysis/scl_response.R
@@ -1,133 +1,194 @@
 
 #################################################################
-##                          Model SCL                          ##
+##                        Model tonic                        ##
 #################################################################
 
+# Load packages -----------------------------------------------------------
 
-# Load Packages -----------------------------------------------------------
-
-library(tidyverse) # Easily Install and Load the 'Tidyverse'
-library(lmerTest) # Tests in Linear Mixed Effects Models
 library(emmeans) # Estimated Marginal Means, aka Least-Squares Means
-source("code/apa_f.R") # Get APA summary of fixed effects
+library(tidyverse) # Easily Install and Load the 'Tidyverse'
+library(lmerTest) # Linear Mixed-Effects Models using 'Eigen' and S4
+library(AICcmodavg) # Model Selection and Multimodel Inference Based on (Q)AIC(c)
+source("code/apa_f.r")
+
 # Import Data -------------------------------------------------------------
 
-scl_data <- here::here(
+tonic_all <-here::here(
   "data",
   "all_data.Rds"
 ) %>%
   read_rds() %>%
   mutate(
     liking = liking - 3
-  ) %>%
-  filter(abs(tonic_z) < 3) %>% # remove scores above/below 3SDs
-  drop_na(liking, tas_z)
+  ) %>% 
+  filter(
+    abs(tonic_z) < 3
+  )
 
-length(unique(scl_data$pid))
-
-log <- here::here(
+tonic_log <- here::here(
   "data",
   "log.csv"
 ) %>%
   read_csv() %>%
   rename(
     pid = P
-  )
-
-
-# Get bad recordings ------------------------------------------------------
-
-bad_tonic <- log %>%
-  filter(!is.na(Eda)) %>%
+  ) %>% 
+  filter(
+    grepl("bad", Eda)
+  ) %>%
   select(pid)
 
-# Create tonic data --------------------------------------------------------
+tonic <- tonic_all %>% 
+  anti_join(tonic_log) %>% 
+  filter(abs(tonic_z) < 3) %>% 
+  drop_na(liking, tas_z) %>% 
+  mutate(
+    log_scl = log(tonic + 1)
+  ) 
 
-tonic <- scl_data %>%
-  anti_join(bad_tonic,
-    by = "pid"
-  )
 
-length(unique(scl_data$pid)) - length(unique(tonic$pid)) # 14 cases removed
-
+length(unique(tonic$pid))
 # Model Responses ---------------------------------------------------------
 
-# Model 1: Main Effect of Factor ------------------------------------------
+# Fit data to null model 
 
-
-tonic_1 <- lmerTest::lmer(
-  tonic_z ~ factor + (1 + factor | pid) + (1 | song),
+tonic_null <- lmerTest::lmer(
+  tonic ~ 1 + (1 | pid),
   data = tonic,
-  contrasts = list(
-    factor = contr.sum
-  ),
   REML = FALSE
-) # singulatrity warning but this is owing to person as the standarised scores are being used. The random slope and ranfom intercept of song allow a better fit acording to the AIC
-
-
-# Model 2: Add liking -----------------------------------------------------
-tonic_2 <- update(
-  tonic_1,
-  . ~ . + liking
 )
 
-# Model 3 Add Interactions ------------------------------------------------
+tonic_rand <- lmerTest::lmer(
+  tonic ~ 1 + (1 | pid) + (1 | song),
+  data = tonic,
+  REML = FALSE
+)
+
+anova(tonic_null, tonic_rand) # null model is prefered
+
+# Random structure is best 
+# Model 1: Liking ------------------------------------------
+
+
+tonic_1 <- update(
+  tonic_null, 
+  . ~ . + liking
+)
+# Model 2: Add factor -----------------------------------------------------
+tonic_2 <- update(
+  tonic_1,
+  . ~ . + factor
+)
+
+# Model 4 Add interaction ------------------------------------------------
 
 tonic_3 <- update(
   tonic_2,
   . ~ . + liking:factor
 )
-# Model 4 add TAS ---------------------------------------------------------
+# Model 5 add TAS ---------------------------------------------------------
 
 tonic_4 <- update(
   tonic_3,
-  . ~ . + tas_z + tas_z:factor
+  . ~ . + tas_z
 )
 
-
-# Model 5 add depression --------------------------------------------------
 
 tonic_5 <- update(
   tonic_4,
-  . ~ . + depression_z + depression_z:factor
-) # model failed to converge
+  . ~ . + tas_z:factor + tas_z:liking
+)
+# Model 6 add depression --------------------------------------------------
 
+tonic_5 <- update(
+  tonic_4,
+  . ~ . + depression_z 
+)
 
-
-# ANOVA of models ---------------------------------------------------------
-
-anova(tonic_1, tonic_2, tonic_3, tonic_4, tonic_5)
-# Compare Model Fits ------------------------------------------------------
-
-sapply(
-  paste0(
-    "tonic_",
-    c(1:5)
-  ),
-  function(x) {
-    AIC(get(x))
-  }
-) %>%
-  data.frame() %>%
-  rename(
-    AIC = "."
-  ) %>%
-  mutate(
-    AICdiff = AIC - AIC(tonic_5)
-  ) # considewring parsimony model 1 is the best model
-
-# Get anova table ---------------------------------------------------------
-
-apa_f(tonic_1)
-
-
-
-# Plot effect -------------------------------------------------------------
-
-sjPlot::plot_model(
-  tonic_1,
-  type = "pred",
-  terms = "factor"
+tonic_6 <- update(
+  tonic_5,
+  . ~ . + depression_z:factor + depression_z:liking
 )
 
 
+# LRT of model fit ------------------------------------------------------
+
+anova(tonic_1, tonic_2, tonic_3, tonic_4, tonic_5, tonic_6)
+
+# ANOVA of Model 3 --------------------------------------------------------
+
+apa_f(tonic_3)
+
+
+
+# Estimated Marginal Means ------------------------------------------------
+
+emmeans::emmeans(
+  tonic_3, 
+  pairwise ~ factor, 
+  infer = TRUE
+)
+
+
+
+# Simple Slopes -----------------------------------------------------------
+
+emmeans::emtrends(
+  tonic_3,
+  pairwise ~ factor, 
+  var = "liking"
+)
+# Plot --------------------------------------------------------------------
+
+tonic_plot <- emmeans::emmip(
+  tonic_3,
+  factor ~ liking,
+  CIs = TRUE,
+  at = list(liking = c(-2, 0 , 2))
+) 
+
+
+# Discordance -------------------------------------------------------------
+
+discordacne_null <- lmerTest::lmer(
+  arousal_rating ~ 1 + (1 | pid),
+  data = tonic,
+  REML = FALSE
+)
+
+discordance_random <- lmerTest::lmer(
+  arousal_rating ~ 1 + (1 + factor | pid) + (1 | song),
+  data = tonic,
+  REML = FALSE
+)
+anova(discordacne_null, discordance_random) # random is better
+
+discordance_1 <- update(
+  discordance_random, 
+  . ~ . + tonic
+)
+
+discordance_2 <- update(
+  discordance_1, 
+  . ~ . + tonic:liking
+)
+
+discordance_3 <- update(
+  discordance_2,
+  . ~ . + tas_z
+) 
+
+discordance_4 <- update(
+  discordance_3, 
+  . ~ . + tas_z:tonic + tas_z:liking
+)
+
+anova(discordance_1, discordance_2, discordance_3, discordance_4)
+
+
+emmip(
+  discordance_4,
+  tas_z ~ liking, 
+  at = list(liking = c(-2, 0, 2),
+            tas_z = c(-1, 0, 1))
+)
